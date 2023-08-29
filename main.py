0dd48e1f0c82bee60228156190ac276c509525e0
Alexey <operating.m1@gmail.com>
2023-08-29 19:09:44 +0700

    add table and input info

--- main.py
+++ main.py
@@ -8,18 +8,42 @@ try:
         password=password,
         database=db_name
     )
+    connection.autocommit = True
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+        print(f"Server version: {cursor.fetchone()}")
 
-    cursor = connection.cursor()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                CREATE TABLE c(
+                    id serial PRIMARY KEY,
+                    first_name varchar(50) NOT NULL,
+                    second_name varchar(50) NOT NULL);
+                """
+            )
+            print("[INFO] Table created successfull")
+    except:
+        print("[INFO] Table exist")
 
-    cursor.execute(
-        "SELECT version();"
-    )
-    print(f"Server version: {cursor.fetchone()}")
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            INSERT INTO students (first_name, second_name) VALUES
+            ('Ivan', 'Ivanov'),
+            ('Nikolay', 'Nikolaev');
+            """
+        )
+
+
+
 
 except Exception as _ex:
     print("[INFO] Error while working with PostgreSQL", _ex)
 finally:
     if connection:
-        cursor.close()
         connection.close()
         print("[INFO] PostgreSQL connection closed")
