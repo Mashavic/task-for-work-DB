6de19bc3545389da911d3c97023f678cf30e2c26
Alexey <operating.m1@gmail.com>
2023-09-01 09:20:13 +0700

    "edit open table two"

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class Tb(QTableWidget):
     def __init__(self):
         super().__init__()
         self.connect_data_base()
-        self.setGeometry(0, 0, 500, 800)
+        self.setGeometry(0, 0, 800, 800)
         self.setWindowTitle('Data Base #start_table')
         self.setColumnCount(4)
         self.verticalHeader().hide()
@@ -26,7 +26,7 @@ class Tb(QTableWidget):
             host="127.0.0.1",
             user="postgres",
             password="qwerty123",
-            database="postgres",
+            database="postgres"
         )
         self.cursor_view = self.connection.cursor()
         self.connection.autocommit = True
@@ -51,18 +51,42 @@ class Tb(QTableWidget):
         self.second_form = SecondForm()
 
 
-class SecondForm(QDialog):
+class SecondForm(QTableWidget):
     def __init__(self):
+        self.connect_data_base()
         super().__init__()
-        self.init_ui()
-
-    def init_ui(self):
-        self.setGeometry(400, 400, 400, 400)
+        self.setGeometry(400, 400, 800, 800)
         self.setWindowTitle('Вторая форма')
-        self.setModal(True)
+        self.setColumnCount(4)
+        self.verticalHeader().hide()
+        self.updt_new()  # обновить таблицу
         self.show()
 
+    def connect_data_base(self):
+        self.connection = psycopg2.connect(
+            host="127.0.0.1",
+            user="postgres",
+            password="qwerty123",
+            database="postgres"
+        )
+        self.cursor_view = self.connection.cursor()
+        self.connection.autocommit = True
 
+    def updt_new(self):
+        self.clear()
+        self.setRowCount(0)
+        self.setHorizontalHeaderLabels(['payment_id', 'customer_id', 'amount', 'payment_date'])  # заголовки столцов
+        self.cursor_view.execute("SELECT * FROM payment")
+        rows = self.cursor_view.fetchall()
+        i = 0
+        for elem in rows:
+            self.setRowCount(self.rowCount() + 1)
+            j = 0
+            for t in elem:  # заполняем внутри строки
+                self.setItem(i, j, QTableWidgetItem(str(t).strip()))
+                j += 1
+            i += 1
+        self.resizeColumnsToContents()
 def main():
     try:
         app = QApplication(sys.argv)
